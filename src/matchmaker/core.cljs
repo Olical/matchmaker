9887a4197fcf464912c8c44c68a7d7b7ad82c68a
Oliver Caldwell <olliec87@gmail.com>
2016-08-24 21:35:10 +0100

    Planned skeleton of functions / views

--- src/matchmaker/core.cljs
+++ src/matchmaker/core.cljs
@@ -1,17 +1,40 @@
 (ns matchmaker.core
     (:require [reagent.core :as reagent :refer [atom]]))
 
+(defn parse-players [txt]
+  [])
+
+(defn parse-attendance [txt]
+  [])
+
+(defn can-play [players attendance]
+  [])
+
+(defn group-teams [players]
+  [])
+
 ;; -------------------------
 ;; Views
 
-(defn home-page []
-  [:div [:h2 "Welcome to Reagent"]])
+(defn editor [value]
+  [:textarea {:value @value
+              :on-change #(reset! value (-> % .-target .-value))}])
+
+(defn root []
+  (let [players-txt (atom "")
+        attendance-txt (atom "")]
+    [[(editor players-txt)]
+     [(editor attendance-txt)]
+     (let [players (parse-players players-txt)
+           attendance (parse-attendance attendance-txt)
+           teams (group-teams (can-play players attendance))]
+       [:p "TODO: Everything."])]))
 
 ;; -------------------------
 ;; Initialize app
 
 (defn mount-root []
-  (reagent/render [home-page] (.getElementById js/document "app")))
+  (reagent/render [root] (.getElementById js/document "app")))
 
 (defn init! []
   (mount-root))
