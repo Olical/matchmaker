3fbfa52e9059520ab986b28bc735204fff2d5779
Oliver Caldwell <olliec87@gmail.com>
2016-08-24 23:16:47 +0100

    Parsing of players, attendance and filtering

--- src/matchmaker/core.cljs
+++ src/matchmaker/core.cljs
@@ -1,14 +1,27 @@
 (ns matchmaker.core
-    (:require [reagent.core :as reagent :refer [atom]]))
+    (:require [reagent.core :as reagent :refer [atom]]
+              [clojure.string :refer [split]]))
 
-(defn parse-players [txt]
-  [])
+(def players-tsv (atom ""))
+(def attendance-tsv (atom ""))
 
-(defn parse-attendance [txt]
-  [])
+(defn columns [tsv]
+  (let [rows (split tsv #"\n")
+        cols (map #(split % #"\t") rows)]
+    cols))
+
+(defn parse-players [tsv]
+  (map (fn [c]
+           {:name (nth c 0)
+            :rank (nth c 3 nil)})
+       (columns tsv)))
+
+(defn parse-attendance [tsv]
+  (let [pairs (map (fn [c] [(nth c 0) (= (nth c 1 nil) "attended")]) (columns tsv))]
+    (into (sorted-map) pairs)))
 
 (defn can-play [players attendance]
-  [])
+  (filter (fn [p] (attendance (:name p))) players))
 
 (defn group-teams [players]
   [])
@@ -16,21 +29,19 @@
 ;; -------------------------
 ;; Views
 
-(defn editor [value]
-  [:textarea {:value @value
+(defn editor [placeholder value]
+  [:textarea {:placeholder placeholder
+              :value @value
               :on-change #(reset! value (-> % .-target .-value))}])
-
-(def players-txt (atom ""))
-(def attendance-txt (atom ""))
 
 (defn root []
   [:div
-   [editor players-txt]
-   [editor attendance-txt]
-   (let [players (parse-players players-txt)
-         attendance (parse-attendance attendance-txt)
+   [editor "Players" players-tsv]
+   [editor "Attendance" attendance-tsv]
+   (let [players (parse-players players-tsv)
+         attendance (parse-attendance attendance-tsv)
          teams (group-teams (can-play players attendance))]
-     [:p "TODO: Everything. " (count @players-txt) " - " (count @attendance-txt)])])
+     [:p "TODO: Everything. " (count @players-tsv) " - " (count @attendance-tsv)])])
 
 ;; -------------------------
 ;; Initialize app
